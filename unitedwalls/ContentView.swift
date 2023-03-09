f16bf2b1b5a377296797a83366ab7068cfd8f4d9
Paras Khanchandani <paras_theloveable@hotmail.com>
2023-03-08 21:08:48 -0500

    Initial Commit

---
+++ unitedwalls/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  unitedwalls
+//
+//  Created by Paras KCD on 2023-03-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ unitedwalls/unitedwallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  unitedwallsApp.swift
+//  unitedwalls
+//
+//  Created by Paras KCD on 2023-03-08.
+//
+
+import SwiftUI
+
+@main
+struct unitedwallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
